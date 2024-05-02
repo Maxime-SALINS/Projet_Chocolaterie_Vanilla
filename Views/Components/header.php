1ac00b18c21f0199ea618e596f9faca9082837e9
Maxime <139247550+Maxime-SALINS@users.noreply.github.com>
2024-05-02 10:52:05 +0200

    Start header

---
+++ Views/Components/header.php
@@ -0,0 +1,23 @@
+<?php
+$title_default = 'Page | Chocolaterie';
+$title = 'Page | Accueil';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= !empty($title) ? $title : $title_default ?></title>
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+
+    </header>
+    
+    <footer>
+
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+require 'Views/Components/header.php';
