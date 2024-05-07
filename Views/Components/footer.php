b0c0a991258b742adc6e84bd9c27e791e257f0bb
Maxime <139247550+Maxime-SALINS@users.noreply.github.com>
2024-05-07 17:40:58 +0200

    Update footer

--- Views/Components/footer.php
+++ Views/Components/footer.php
@@ -1,6 +1,78 @@
-<footer>
-
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <footer class="footer-style container-fluid p-2">
+        <div class="row my-3">
+            <h2 class="titre-footer text-center">Abonnez vous à la Newsletter</h2>
+            <form class="row container-fluid text-center" action="" method="post">
+                <div class="col-auto my-1">
+                    <input class="form-control" type="email" name="useremail_news" id="useremail_news" placeholder="Entrez votre email">
+                </div>
+                <div class="col-auto text-start my-1">
+                    <button class="btn text-white" type="submit">Envoyer</button>
+                    <img src="../../assets/images/logo/Image-newsletter.png" alt="">
+                </div>
+            </form>
+        </div>
+        <div class="row my-5">
+            <div class="col-12 col-md-4">
+                <div class="container-fluid text-center">
+                    <h3 class="titre-footer text-uppercase text-decoration-underline"">Sommaire</h3>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="lien-footer nav-link" href="index.php">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="lien-footer nav-link" href="#">Produits</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="lien-footer nav-link" href="#">Histoire</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="container-fluid text-center">
+                    <h3 class="titre-footer text-uppercase text-decoration-underline"">Infomations</h3>
+                    <ul class="nav flex-column">
+                        <li>
+                            <a class="lien-footer nav-link" href="#">Mention légales</a>
+                        </li>
+                        <li>
+                            <a class="lien-footer nav-link" href="#">Cookies</a>
+                        </li>
+                    </ul>
+                    <h4 class="titre-footer">Horaires de la boutique :</h4>
+                    <ul class="list-unstyled">
+                        <li class="lien-footer">Lundi au vendredi : 8h à 18h</li>
+                        <li class="lien-footer">Samedi : 8h à 14h</li>
+                        <li class="lien-footer">Dimanche : fermé</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="container-fluid text-center">
+                    <h3 class="titre-footer text-uppercase text-decoration-underline">Contact</h3>
+                    <div class="container-fluid">
+                        <p class="lien-footer">N° de téléphone: 06 29 88 26 23</p>
+                        <p class="lien-footer">Email: chocolaterie.larouviere@gmail.com</p>
+                        <a class="lien-footer" href="#" target="_blank" rel="noopener noreferrer">Contactez-nous !</a>
+                    </div>
+                    <div class="container-fluid">
+                        <h4 class="titre-footer">Nos réseaux:</h4>
+                        <div class="d-flex justify-content-around">
+                            <a href="#" target="_blank" rel="noopener noreferrer">
+                                <img src="../../assets/images/logo/Instagram-logo.png" alt="logo instagram">
+                            </a>
+                            <a href="" target="_blank" rel="noopener noreferrer">
+                                <img src="../../assets/images/logo/facebook-logo.png" alt="logo facebook">
+                            </a>
+                            <a href="" target="_blank" rel="noopener noreferrer">
+                                <img src="../../assets/images/logo/logo-linkedin.png" alt="logo Linkedin">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
