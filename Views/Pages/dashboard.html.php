4e6034e8859373dadd86ff8a3cc81917475f8b9d
Maxime <139247550+Maxime-SALINS@users.noreply.github.com>
2024-06-03 22:54:20 +0200

    Update dashboard/Profil user

--- Views/Pages/dashboard.html.php
+++ Views/Pages/dashboard.html.php
@@ -2,10 +2,8 @@
     <div class="container text-center">
         <h1 class="my-3">Dashboard administrateur</h1>
     </div>
-    <div class="d-flex align-item-center justify-content-end">
-        <div class="container-fluid text-center">
-            <p class="m-0">Bienvenue <?= $_SESSION['name'] ?> !</p>
-        </div>
+    <div class="d-flex align-item-center justify-content-center">
+        <p class="mx-2">Bienvenue <?= $_SESSION['name'] ?> !</p>
     </div>
 </header>
 <main class="row">
@@ -58,13 +56,48 @@
     <section class="col-10 bg-body-secondary p-3">
         <h2 class="text-uppercase text-center">Profil</h2>
         <div class="row">
-            <section class="col-12">
-                <h3>Informations du profil</h3>
+            <section class="col-4 card mx-auto">
+                <h3 class="text-center my-3">Informations du profil</h3>
                 <ul class="container-fluid">
-                    <li>Prénom : <?= $_SESSION['name'] ?></li>
-                    <li>Email : <?= $_SESSION['email'] ?></li>
-                    <li>Rôle : <?= $_SESSION['role'] ?> </li>
+                    <li class="list-group-item my-2">Prénom : <?= $_SESSION['name'] ?></li>
+                    <li class="list-group-item my-2">Email : <?= $_SESSION['email'] ?></li>
+                    <li class="list-group-item my-2">Rôle : <?= $_SESSION['role'] ?> </li>
                 </ul>
+            </section>
+            <section class="col-4 card mx-auto text-center">
+                <h3 class="text-center my-3">Modification du profil</h3>
+                <form action="" method="post">
+                    <div class="container my-4">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <label for="FormControlInputEmail" class="form-label mx-5">Prénom</label>
+                            <button class="btn btn-success" type="button" id="btn-name">
+                                <i class="bi bi-pencil-fill"></i>
+                            </button>
+                        </div>
+                        <input class="form-control d-none my-2" type="text" name="username" id="username" placeholder="Entrez votre prénom">
+                    </div>
+                    <div class="container my-4">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <label for="FormControlInputEmail" class="form-label mx-5">Email</label>
+                            <button class="btn btn-success" type="button" id="btn-email">
+                                <i class="bi bi-pencil-fill"></i>
+                            </button>
+                        </div>
+                        <input class="form-control d-none my-2" type="email" name="email" id="email" placeholder="Entrez votre nouvel email">
+                    </div>
+                    <div class="container my-4">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <label for="FormControlInputpassword" class="form-label mx-5">Mot de passe</label>
+                            <button class="btn btn-success" type="button" id="btn-password">
+                                <i class="bi bi-pencil-fill"></i>
+                            </button>
+                        </div>
+                        <input class="form-control d-none my-2" type="password" name="password" id="password" placeholder="Entrez votre nouveau mot de passe">
+                    </div>
+                    <div class="container my-3">
+                        <button type="submit" class="btn btn-primary">Valider</button>
+                    </div>
+                </form>
             </section>
         </div>
     </section>
